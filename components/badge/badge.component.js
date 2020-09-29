acd8091b99d02a7c558e807c171351a55468c291
knowthelist <mstephan@shared-files.de>
2020-09-29 22:19:34 +0200

    fixed typo

--- components/badge/badge.component.js
+++ components/badge/badge.component.js
@@ -1,5 +1,5 @@
 /* 
-* Dropdown component for FTUI version 3
+* Badge component for FTUI version 3
 *
 * Copyright (c) 2020 Mario Stephan <mstephan@shared-files.de>
 * Under MIT License (http://www.opensource.org/licenses/mit-license.php)
