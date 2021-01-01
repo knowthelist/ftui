5fb64ca15df57ddd3caf37ddadcf3aaedd10232d
knowthelist <mstephan@shared-files.de>
2021-01-01 22:26:11 +0100

    updated control file

--- prepare_update.sh
+++ prepare_update.sh
@@ -6,9 +6,8 @@ git ls-files -z | while IFS= read -r -d '' f;
   do
     if [[ $f != *.git* && $f != *.eslintrc* && $f == *www/ftui* ]]; then
       out="$(stat -f "%Sm" -t "%Y-%m-%d_%T" $f) "$(stat -f%z $f)" ${f}"
-      if [[ $f == *index.html* ]]; then
-        echo CRE ${out//.\//} >> update_ftui.txt
-      else
+      echo CRE ${out//.\//} >> update_ftui.txt
+      if [[ $f != *index.html* ]]; then
         echo UPD ${out//.\//} >> update_ftui.txt
       fi
     fi 
