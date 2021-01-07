2efda2de0f1d45615f41b4a015dd1c78aa10f66b
knowthelist <mstephan@shared-files.de>
2021-01-07 03:14:02 +0100

    easier weather map

--- www/ftui/components/weather/weather.component.js
+++ www/ftui/components/weather/weather.component.js
@@ -8,7 +8,7 @@
 */
 
 import { FtuiIcon } from '../icon/icon.component.js';
-import providerSets from './weather.map.js';
+import { providerSet, iconSet } from './weather.map.js';
 
 class FtuiWeather extends FtuiIcon {
 
@@ -34,9 +34,9 @@ class FtuiWeather extends FtuiIcon {
   onAttributeChanged(name, newValue) {
     switch (name) {
       case 'condition': {
-        const iconSets = providerSets[this.provider] || {};
-        const icons = iconSets[this.iconSet] || {};
-        this.loadIcon(icons[newValue] || 'icons/none.svg');
+        const conditions = providerSet[this.provider] || {};
+        const icons = iconSet[this.iconSet] || {};
+        this.loadIcon(icons[conditions[newValue]] || 'icons/none.svg');
         break;
       }
       default:
