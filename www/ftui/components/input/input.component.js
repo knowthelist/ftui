5a23b1b8ce4455f2c67b593ef412080854699f6e
knowthelist <mstephan@shared-files.de>
2021-11-05 00:57:18 +0100

    added input and meter

---
+++ www/ftui/components/input/input.component.js
@@ -0,0 +1,93 @@
+/*
+* Input component
+*
+* for FTUI version 3
+*
+* Copyright (c) 2021 Mario Stephan <mstephan@shared-files.de>
+* Under MIT License (http://www.opensource.org/licenses/mit-license.php)
+*
+* https://github.com/knowthelist/ftui
+*/
+
+import { FtuiElement } from '../element.component.js';
+
+export class FtuiInput extends FtuiElement {
+
+  constructor() {
+    super(FtuiInput.properties);
+
+    this.input = this.shadowRoot.querySelector('input');
+    this.input.addEventListener('keyup', (e) => this.onEnter(e));
+    this.input.addEventListener('change', () => this.onChange());
+  }
+
+  template() {
+    return `
+          <style>
+          input {
+            border-radius: 0.75em;
+            border: 2px solid var(--border-color);
+            background: transparent;
+            padding: 0.4em;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            appearance: none;
+            font-size: 1em;
+            color: var(--text-color);  
+            outline: none;
+            cursor: pointer;
+          }
+          input:invalid { border: solid 2px var(--red); }
+          </style>
+          <input type="${this.type}"
+            min="${this.min}" 
+            minLength="${this.minLength}" 
+            max="${this.max}" 
+            maxLength="${this.maxLength}" 
+            value="${this.value}" 
+            placeholder="${this.placeholder}" 
+            pattern="${this.pattern}" 
+            height="${this.height}" 
+            width="${this.width}">`;
+  }
+
+  static get properties() {
+    return {
+      value: '',
+      type: 'text',
+      min: '',
+      minLength: '',
+      max: '',
+      maxLength: '',
+      step: 1,
+      placeholder: '',
+      pattern: '.*',
+      height: '',
+      width: '',
+      debounce: 400,
+    };
+  }
+
+  static get observedAttributes() {
+    return [...this.convertToAttributes(FtuiInput.properties), ...super.observedAttributes];
+  }
+
+  onAttributeChanged(name, value) {
+    this.input[name] = value;
+  }
+
+  onEnter(event) {
+    if (event.keyCode === 13 && this.input.validity.valid) {
+      this.submitChange('value', this.input.value);
+      if (this.hasAttribute('autoclear')) {
+        this.input.value = '';
+      }
+    }
+  }
+
+  onChange() {
+    this.submitChange('value', this.input.value);
+  }
+}
+
+window.customElements.define('ftui-input', FtuiInput);
---
+++ www/ftui/components/meter/meter.component.js
@@ -0,0 +1,84 @@
+/*
+* Meter component
+*
+* for FTUI version 3
+*
+* Copyright (c) 2021 Mario Stephan <mstephan@shared-files.de>
+* Under MIT License (http://www.opensource.org/licenses/mit-license.php)
+*
+* https://github.com/knowthelist/ftui
+*/
+
+import { FtuiElement } from '../element.component.js';
+
+export class FtuiMeter extends FtuiElement {
+
+  constructor() {
+    super(FtuiMeter.properties);
+
+    this.progress = this.shadowRoot.querySelector('.progress');
+    this.bar = this.shadowRoot.querySelector('.progress-bar');
+
+  }
+
+  template() {
+    return `
+          <style>
+          .container {
+            width: 100%;
+            text-align: center;
+          }
+          .progress {
+            width: ${this.width};
+            height: ${this.height};
+            padding: 0.15em 0 0.07em 0.1em;
+            background: var(--dark-color);
+            border-radius: 6px;
+            -webkit-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.25), 0 1px rgba(255, 255, 255, 0.08);
+            box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.25), 0 1px rgba(255, 255, 255, 0.08);
+          }
+          .progress-bar {
+            height: 100%;
+            background-color: var(--color-base, #20639b);
+            border-radius: 4px;
+            -webkit-transition: 0.4s linear;
+            transition: 0.4s linear;
+            -webkit-transition-property: width, background-color;
+            transition-property: width, background-color;
+            -webkit-box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.25), inset 0 1px rgba(255, 255, 255, 0.1);
+            box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.25), inset 0 1px rgba(255, 255, 255, 0.1);
+          }
+            </style>
+            <div class="container">
+              <div class="progress">
+                <div class="progress-bar"></div>
+                <slot></slot>
+              </div>
+            </div>`;
+
+  }
+
+  static get properties() {
+    return {
+      height: '1em',
+      width: '10em',
+      color: 'primary',
+      value: 0,
+    };
+  }
+
+  static get observedAttributes() {
+    return [...this.convertToAttributes(FtuiMeter.properties), ...super.observedAttributes];
+  }
+
+  onAttributeChanged(name) {
+    switch (name) {
+      case 'value':
+        this.bar.style.width = this.value + '%';
+        break;
+    }
+  }
+
+}
+
+window.customElements.define('ftui-meter', FtuiMeter);
