da09242df1b37eaa70ca815d921403be6e3f9c2f
Harald Roelle <harald.roelle@gmail.com>
2022-02-23 14:27:40 +0100

    Remove debug logging

--- www/ftui/modules/ftui/ftui.helper.js
+++ www/ftui/modules/ftui/ftui.helper.js
@@ -107,7 +107,6 @@ export function getFilteredStepKeys(map, searchKey) {
 // Path calculation functions
 
 export function getLocalCssPath(inClassFileUrl, inFileName = null) {
-  console.log("getLocalCssPath: " + ((new URL(inClassFileUrl)).pathname.replace(/.js$/, '.css')));
   if (inFileName == null)
     return ((new URL(inClassFileUrl)).pathname.replace(/.js$/, '.css'));
   else
