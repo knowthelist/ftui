0ddc3cd7231cf72cbc9257ffa8bd821422f5c206
knowthelist <mstephan@shared-files.de>
2025-03-06 13:33:49 +0100

    update weather bar component styles and functionality; adjust background color, add unit display, and enhance examples

--- www/ftui/components/weather/weather-bar.component.js
+++ www/ftui/components/weather/weather-bar.component.js
@@ -6,19 +6,23 @@ class FtuiWeatherBar extends FtuiElement {
 
     super(Object.assign(FtuiWeatherBar.properties, properties));
 
+    this.minLabel = this.shadowRoot.querySelector('.min');
+    this.maxLabel = this.shadowRoot.querySelector('.max');
     this.progress = this.shadowRoot.querySelector('.progress');
-    this.progress.style.width = this.width || (this.isVertical ? '1em' : '7em');
-    this.progress.style.height = this.height || (this.isVertical ? '7em' : '1em');
+    this.progress.style.width = this.width || '7em';
+    this.progress.style.height = this.height || '1em';
     this.progressBar = this.shadowRoot.querySelector('.progress-bar');
   }
 
   static get properties() {
     return {
+      height: '',
+      width: '',
       minTemp: 10,
       maxTemp: 20,
       min: 0,
       max: 30,
-      isVertical: false,
+      unit: '°C',
     };
   }
 
@@ -34,27 +38,30 @@ class FtuiWeatherBar extends FtuiElement {
         </div>`;
   }
 
+  static get observedAttributes() {
+    return [...this.convertToAttributes(FtuiWeatherBar.properties), ...super.observedAttributes];
+  }
   onAttributeChanged(name, oldValue, newValue) {
     this.updateProgress();
   }
 
   updateProgress() {
-    console.log('updateProgress');
     const minTemp = scale(this.minTemp, this.min, this.max, 0, 100);
     const width = scale(this.maxTemp, this.min, this.max, 0, 100) - minTemp;
     this.progressBar.style.left = `${minTemp}%`;
     this.progressBar.style.width = `${width}%`;
-        // Calculate gradient stops
-        const blueStop = ((5 -  this.minTemp) / (this.maxTemp - this.minTemp)) * 100;
-        const yellowStop = ((20 - this.minTemp) / (this.maxTemp -this. minTemp)) * 100;
-    
-        this.progressBar.style.background = `linear-gradient(to right, 
+    // Calculate gradient stops
+    const blueStop = ((5 - this.minTemp) / (this.maxTemp - this.minTemp)) * 100;
+    const yellowStop = ((20 - this.minTemp) / (this.maxTemp - this.minTemp)) * 100;
+    this.progressBar.style.background = `linear-gradient(to right, 
         lightblue 0%, 
         lightblue ${blueStop}%, 
-        #bdbd79 ${blueStop*2}%, 
+        #bdbd79 ${blueStop * 2}%, 
         #bdbd79 ${yellowStop}%, 
         #f28383 100%)`;
-   
+    // update labels
+    this.minLabel.textContent = `${this.minTemp} ${this.unit}`;
+    this.maxLabel.textContent = `${this.maxTemp} ${this.unit}`;
   }
 }
 
